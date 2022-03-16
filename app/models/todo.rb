fa587157386c010876279cffeffa037162c5205e
Hopsoft <natehop@gmail.com>
2022-03-15 19:02:52 -0600

    Remove comment noise for diff

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: todos
-#
-#  id         :bigint           not null, primary key
-#  session_id :string           not null
-#  title      :string           not null
-#  completed  :boolean          default(FALSE), not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Todo < ApplicationRecord
   validates :session_id, presence: true
   validates :title, presence: true
